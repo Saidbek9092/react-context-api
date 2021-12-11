8b4ba7801c494878870c36a1a71bf51141baec5a
Saidbek9092 <saidbek9092@gmail.com>
2021-12-11 23:02:43 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <div className="App">
                 {/*<Nav/>*/}
                 {/*<AddMovie/>*/}
-                <MoviesList/>
+                {/*<MoviesList/>*/}
             </div>
         </MovieProvider>
     );
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -9,11 +9,22 @@ import classes from "./movies.sass"
 const MoviesList = () => {
     const [ movies, setMovies ] = useContext( MovieContext )
     const [ show, setShow ] = useState( false )
+    const [info, setInfo] = useState([{name: "Saidbek", age: 22}, {name: "Bob", age: 15}])
+
+    const add = () => {
+        const newinfo = {name: 'google', age: 12}
+        setInfo([
+            ...info,
+            newinfo
+        ])
+    }
     return (
         <div>
             <div>
                 <h1 className={classes.h1}>Saidbek</h1>
                 <h2>New changes</h2>
+                <button onClick={add}>Press it </button>
+                <button onClick={()=> console.log(info)}>show </button>
                 {/*{ movies.map( ( movie, index ) => <Movie data={ movie } key={ index }/> ) }*/}
                 {/*<button onClick={ () => setTimeout( () => setShow( true ), 2000 ) }>Show movies</button>*/}
                 {/*{ show ? <OldMovies/> : <h2>Press Show movies again</h2> }*/}
